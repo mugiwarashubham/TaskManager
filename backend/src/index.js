67e18b7c2f539fc94593b8267d01010c3a23c977
shubham <shubhamsinghala20@gmail.com>
2026-06-04 01:50:24 +0530

    deployment error fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,22 +1,36 @@
 require("dotenv").config();
 
+console.log("🚀 APP STARTED");
+console.log("PORT =", process.env.PORT);
+console.log(
+  "DB_CONNECT_STRING =",
+  process.env.DB_CONNECT_STRING ? "FOUND" : "MISSING"
+);
+
 const express = require("express");
 const cors = require("cors");
 
-const main = require('./config/db');
-
+const main = require("./config/db");
 const taskRoutes = require("./routes/taskroutes");
 
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
 
 app.use("/tasks", taskRoutes);
 
-main().then(() => {
-  app.listen(process.env.PORT, () => {
-    console.log("Server listening at"+process.env.PORT);
+const PORT = process.env.PORT || 4000;
+
+main()
+  .then(() => {
+    console.log("✅ Database Connected");
+
+    app.listen(PORT, () => {
+      console.log(`✅ Server listening at ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("❌ Database Connection Failed");
+    console.error(error);
   });
-});
